bb106d98f288c211c13a019ac278532903f7683b
keecarter <153045725+keecarter@users.noreply.github.com>
2024-06-04 15:49:09 -0400

    Module 5, exercise 1

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
-import myTestRoutes from "./routes/myTestRoutes.js"; 
-import calculatorRoutes from "./routes/calculatorRoutes.js";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.use("/", express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use("/myTest", myTestRoutes);
-app.use("/calculator", calculatorRoutes);
-  
+app.get("/test", (req, res) => {
+  res.send("This is a test");
+});
+
 app.listen(port, () => {
-    console.log(`myapp listening at http://localhost:${port}`);
-  });
+  console.log(`myapp listening
+at http://localhost:${port}`);
+});
+
+const app2 = express();
+const port2 = 3001; // Different port
+
+app2.get("/", (req, res) => {
+  res.send("Hello World again!");
+});
+
+app2.get("/test", (req, res) => {
+  res.send("This is a another test");
+});
+
+app2.listen(port2, () => {
+  console.log(`App2 listening at http://localhost:${port2}`);
+});
